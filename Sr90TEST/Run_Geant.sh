64f892eaf2b6123199f497823baa6d22227ee0b0
Alexey Dzyuba <a.dzyuba@gmail.com>
2018-07-17 10:14:25 +0300

    typo fixed

--- Sr90TEST/Run_Geant.sh
+++ Sr90TEST/Run_Geant.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm *LOG;
-source /home/user/Sim//geant4.10.04.p02-install/bin/geant4.sh;
+source /home/user/Sim/geant4.10.04.p02-install/bin/geant4.sh;
 make;
-./exec_Sr90TESE run.mac >> inLOG;
+./exec_Sr90TEST run.mac >> inLOG;
