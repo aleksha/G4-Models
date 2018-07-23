4769cc1fc9694602e6ff3e49250253e212b6f6e0
Alexey Dzyuba <a.dzyuba@gmail.com>
2018-07-23 12:47:28 +0300

    make TPC working again

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Models for GEANT-4
 
 Repository contains different GEANT-4 models, which use 
 **_SteppingAction_** class to get detector response. These are:
- + **PROTON** - big TPC equiped with MWPC\CSC;
+ + **PRO** - big TPC equiped with MWPC\CSC;
  + **CSC** - prototipe of the CSC;
- + **ACTAF** - TPC prototype;
+ + **TPC** - TPC prototype (ACTAF);
  + **Sr99TEST** - test of radiational damage of MWPC for CMS
     with the Sr-99 beta source.
 
--- TPC/analysis/Reso/analyze.C
+++ TPC/analysis/Reso/analyze.C
@@ -15,16 +15,15 @@
 #include "TLatex.h"
 
 
-double startTPC = 0. ;
-
-TH1F* hTPC = new TH1F("hTPC"," ;time, 10*ns; energy, a.u.", 2550, 0., 4.*2550. );
-TH1F* hGEN = new TH1F("hGEN"," ;time, 10*ns; energy, a.u.", 2550, 0., 4.*2550. );
-TH1F* hINT = new TH1F("hINT"," ;time, 10*ns; energy, a.u.", 2550, 0., 4.*2550. );
-TH1F* hSig = new TH1F("hSig"," ;energy, a.u.; events", 200, 1., 3. );
-TH1F* hTime = new TH1F("hTime"," ;time, 10*ns; energy, a.u.", 2.*2550, -4.*2550., 4.*2550. );
-
-
 void findTPCtracks(){
+  double startTPC = 0. ;
+
+  TH1F* hTPC = new TH1F("hTPC"," ;time, 10*ns; energy, a.u.", 2550, 0., 4.*2550. );
+  TH1F* hGEN = new TH1F("hGEN"," ;time, 10*ns; energy, a.u.", 2550, 0., 4.*2550. );
+  TH1F* hINT = new TH1F("hINT"," ;time, 10*ns; energy, a.u.", 2550, 0., 4.*2550. );
+  TH1F* hSig = new TH1F("hSig"," ;energy, a.u.; events", 200, 1., 3. );
+  TH1F* hTime = new TH1F("hTime"," ;time, 10*ns; energy, a.u.", 2.*2550, -4.*2550., 4.*2550. );
+
 
   hGEN->SetLineColor(2);
   hGEN->SetFillColor(2);
@@ -75,18 +74,18 @@ void findTPCtracks(){
 // loop on trigger event
 //==============================================================================
 
-  std::ifstream frTPC("../../TPC_build/rest.data"      , std::ios::in);
+  std::ifstream frTPC("./rest.data"      , std::ios::in);
 
   int n_ev = 0;
   int EVENT = 0;
-  int process_ev = 100;
+  int process_ev = 10;
   double c_time = -8.*2550.;
   double time_shift = 0.;
 
   double frqs[6] = {    60,    123,    342,    706,   1000,   1500};
-  
-  double freq = 1000.*frqs[2];
-  
+
+  double freq = 1000.*frqs[0];
+
   bool add_gen     = true ;
   bool calc_energy = true ;
   double sum   = 0;
@@ -104,7 +103,7 @@ void findTPCtracks(){
   ev = 0;
   while(n_ev<process_ev){
     frTPC >> ev >> id >> x1 >> y1 >> z1 >> t1 >> x2 >> y2 >> z2 >> t2 >> ed ;
-    
+
     if(ev>EVENT){
       time_shift = gRandom->Exp( 1000.*1000.*100. / freq );
       c_time = c_time + time_shift;
@@ -118,7 +117,7 @@ void findTPCtracks(){
           for(int iii = 0 ; iii<125; iii++  ){
             hTPC->Fill( 6000. + tt , de*Digi[iii]);
             tt = tt + 4 ;
-          }      
+          }
         }
 
         if(calc_energy){
@@ -141,7 +140,7 @@ void findTPCtracks(){
           while(go_left){
             if( hTPC->GetBinContent(kk) < base_l || signal>500){ go_left = false; }
             else{
-              signal = signal + hTPC->GetBinContent(kk) - base_l; 
+              signal = signal + hTPC->GetBinContent(kk) - base_l;
               hGEN->SetBinContent(kk, hTPC->GetBinContent(kk) - base_l );
             }
             kk--;
@@ -161,7 +160,6 @@ void findTPCtracks(){
           std::cout << "   n_ev "<< n_ev ;
           std::cout << "   max " << jj_max << "  " << h_max << "   base " << base_l ;
           std::cout << "   sig " << signal << "\n";
-         
         }
         if(n_ev == -25){
           TCanvas *cnv = new TCanvas("cnv","cnv",900,300);
@@ -177,7 +175,6 @@ void findTPCtracks(){
           hGEN->SetBinContent(jj,0);
           hGEN->SetBinError(jj,0);
         }
-        
         c_time = -8.*2550.;
         n_ev++;
       }
@@ -191,27 +188,20 @@ void findTPCtracks(){
       x = x1 + dx*(0.5+i); y = y1 + dy*(0.5+i); z = z1 + dz*(0.5+i);
       t = t1 + dt*(0.5+i); t_anod = 0.1*(t + (z-z_anod-3.) / W1 + 3./W2 );
       l = sqrt(x*x+y*y);
-  
       if(l>0 && l<10.){
         tt = 2;
         for(int iii = 0 ; iii<125; iii++  ){
           hTPC->Fill(start_delay + t_anod + tt + c_time, de*Digi[iii]);
           hINT->Fill(start_delay + t_anod + tt + c_time, de*Digi[iii]);
           tt = tt + 4 ;
-        }      
-      }    
+        }
+      }
     }
   }
 
-  frTPC.close(); 
-}
+  frTPC.close();
 
-
-TCanvas* canv = new TCanvas("canv","canv",900,600);
-
-void analyze(){
-  findTPCtracks();
-
+  TCanvas* canv = new TCanvas("canv","canv",900,600);
   canv->Divide(1,2);
   canv->cd(1);
   hINT->Draw("hist");
@@ -219,9 +209,25 @@ void analyze(){
   //hGEN->Draw("same");
   canv->cd(2);
   hSig->Draw();
-  
+
+  canv->Print("c.png");
+  canv->Close();
+
 //  hTime->Draw();
 
- // gSystem->Exit(0);
+
+}
+
+
+
+void analyze(){
+
+
+
+
+
+  findTPCtracks();
+
+  gSystem->Exit(0);
 
 }
