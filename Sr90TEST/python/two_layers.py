eb4275417a1e0d359f033c3ca05166312cde8192
Alexey Dzyuba <a.dzyuba@gmail.com>
2018-08-23 15:29:25 +0300

    new python scripts

--- Sr90TEST/python/two_layers.py
+++ Sr90TEST/python/two_layers.py
@@ -9,7 +9,7 @@ from math import pow, sqrt, pi
 #===============================================================================
 dist    = []
 weights = []
-in_file = open("out.data","r")
+in_file = open("g_Cu2.data","r")
 for line in in_file:
     w = line[:-1].split(" ")
 #    if float(w[3])<300 and float(w[3])>30 :
@@ -20,14 +20,32 @@ for line in in_file:
         weights.append( float(w[4]) / (2.*pi*float(w[3])) )
 in_file.close()
 #===============================================================================
+dist2    = []
+weights2 = []
+in_file = open("g_Cu3.data","r")
+for line in in_file:
+    w = line[:-1].split(" ")
+#    if float(w[3])<300 and float(w[3])>30 :
+    if float(w[3])<300 :
+        dist2   .append(float(w[3]))
+#        weights.append(float(w[4]))
+#        weights.append( 1./(2.*pi*float(w[3])) )
+        weights2.append( float(w[4]) / (2.*pi*float(w[3])) )
+in_file.close()
+#===============================================================================
 bins = []
 for ii in range(61):
     bins.append(5.*ii)
 #===============================================================================
+n2, bins2, patches2 = plt.hist( x       = np.array( dist2    ) ,
+                             weights = np.array( weights2 ) ,
+                             bins    = bins    ,
+                             facecolor='blue', alpha=0.5)
+#===============================================================================
 n, bins, patches = plt.hist( x       = np.array( dist    ) ,
                              weights = np.array( weights ) ,
                              bins    = bins    ,
-                             facecolor='green', alpha=0.75)
+                             facecolor='green', alpha=0.5)
 #===============================================================================
 plt.xlabel('Distance, mm')
 plt.ylabel('Dose Density, a.u.')
