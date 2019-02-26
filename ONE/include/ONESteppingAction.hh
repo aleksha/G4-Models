581d9252c783d51022b637f836a52b3ec4eb1af2
Alexey Dzyuba <a.dzyuba@gmail.com>
2019-02-26 05:56:54 +0300

    only one volume in include files

--- ONE/include/ONESteppingAction.hh
+++ ONE/include/ONESteppingAction.hh
@@ -28,27 +28,8 @@ class ONESteppingAction : public G4UserSteppingAction
   private:
     ONEEventAction*  fEventAction;
     std::ofstream myOUT;
-    std::ofstream myINI;
-    std::ofstream myONE;
-    std::ofstream mySCI;
 
     G4LogicalVolume* fLV00;
-    G4LogicalVolume* fLV01;
-    G4LogicalVolume* fLV02;
-    G4LogicalVolume* fLV03;
-    G4LogicalVolume* fLV04;
-    G4LogicalVolume* fLV05;
-    G4LogicalVolume* fLV06;
-    G4LogicalVolume* fLV07;
-    G4LogicalVolume* fLV08;
-    G4LogicalVolume* fLV09;
-    G4LogicalVolume* fLV10;
-    G4LogicalVolume* fLV11;
-    G4LogicalVolume* fLV12;
-    G4LogicalVolume* fLV13;
-    G4LogicalVolume* fLV14;
-    G4LogicalVolume* fLV15;
-    G4LogicalVolume* fLV16;
 };
 //------------------------------------------------------------------------------
 #endif
